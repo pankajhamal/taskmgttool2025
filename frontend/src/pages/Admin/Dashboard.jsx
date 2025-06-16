c0313e848efe914e2d5f3413d70f2dbe6d2da6cd
Pankaj hamal <pankajhamal0@gmail.com>
2025-06-16 14:56:18 +0545

    login and signup

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "../../components/Layouts/DashboardLayout.jsx"
 const Dashboard = () => {
   return (
     <div>
-      
+      <h1>Admin portal</h1>
     </div>
   )
 }
